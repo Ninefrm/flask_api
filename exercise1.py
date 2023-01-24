a195cb4abfb1ca73d7e4d0c830fa5b2672f818ff
Maximiliano Fonseca <maxfonseca.r@gmail.com>
2023-01-24 13:59:00 -0600

    listener /ping

--- exercise1.py
+++ exercise1.py
@@ -8,3 +8,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+# listener / server process that can respond to unauthenticated /ping calls.
+@app.route("/ping")
+def hello_world():
+    return "ping received"
